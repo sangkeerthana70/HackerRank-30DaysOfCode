3d4d0b7c82fe5dae5a6493ae48ecf3465eed273f
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-19 11:59:38 -0500

    Day-4 Class vs Instance

---
+++ Day4/Day4/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class Person
+    {
+        // properties
+        public int age;
+        // constructor 
+        public Person(int initialAge)
+        {
+            if(!(initialAge < 0))
+            {
+                this.age = initialAge;
+            }
+            else
+            {
+                this.age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0");
+            }
+            
+        }
+        // methods
+
+        public void yearPasses(int age)
+        {
+            age++;
+            Console.WriteLine("Age: " + age);
+            
+        }
+
+        public void amIOld(int age)
+        {
+            if(age < 13)
+            {
+                Console.WriteLine("You are young");
+            }
+            if(age >=13 && age <= 18)
+            {
+                Console.WriteLine("You are a teenager");
+            }
+            else
+            {
+                Console.WriteLine("you are old");
+            }
+
+        }
+    }
+}
---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the Person class
+            Person person = new Person(0);
+            // set the new instance of person's age 
+            person.age = 18;
+            // call the method inside Person class
+            person.yearPasses(person.age);
+            person.amIOld(person.age);
+
+            Person a = new Person(-1);
+            a.yearPasses(a.age);
+            
+            Console.WriteLine("now a.age is: " + a.age);
+            a.amIOld(a.age);
+
+            
+
+
+        }
+    }
+}
