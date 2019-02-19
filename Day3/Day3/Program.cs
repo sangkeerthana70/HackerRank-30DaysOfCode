28529b3e42cdf5bd6644f90f0a1632b9d46d0f9b
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-19 11:28:26 -0500

    Day 3 intro to conditional statements

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+
+            if(n % 2 == 0)
+            {
+                if(n > 2 && n <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                if(n > 6 && n <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else
+                {
+                    Console.WriteLine("Not Weird");
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Weird");
+            }
+        }
+    }
+}
