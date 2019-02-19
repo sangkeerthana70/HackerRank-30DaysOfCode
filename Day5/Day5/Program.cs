72164adb35903f06b3683eda79970dc411938aec
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-19 12:32:03 -0500

    Day5 practice on loops

---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            for(var i = 1; i <= 10; i++)
+            {
+                int result = n * i;
+                Console.WriteLine(n + " x " + i + " = " + result);
+            }
+        }
+    }
+}
