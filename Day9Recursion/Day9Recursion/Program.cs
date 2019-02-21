1e25636559e88243829d315a863d42c9084a0e8a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-21 12:56:32 -0500

    Recursion solution

---
+++ Day9Recursion/Day9Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = Factorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static int Factorial(int n)
+        {
+            //int factorial = 1;
+            if( n == 0)
+            {
+                return 1;
+            }
+            
+            
+            return n * Factorial(n - 1); ;
+        }
+    }
+}
