6316a39ac35a80045102d7f91c51289aac8dfd80
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-22 12:30:50 -0500

    Day10 Convert decimal to binary

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -11,62 +11,40 @@ namespace Day10
         static void Main(string[] args)
         {
             int number = 12;
-            
-
-
-
-            //do
-            //{
-
-            //    remainder = number / 2;
-            //    Console.WriteLine("rem: " + remainder);
-
-            //    Console.WriteLine(number % 2);
-            //    continue;
-            //    i++;
-
-            //}
-            //while (!(number % 2 == 0));
-
-            int remainder = number % 2;
-            int quotient = number / 2;
-
-            Console.WriteLine("quo : " + quotient);
-            Console.WriteLine("rem : " + remainder);
-            int i = 0;
-            while (true)
-            {
-                
+            List<int> result = new List<int> ();
+            int quotient = number;
+            int remainder = 0;
+            while (quotient != 0)
+            {               
                 // Continue dividing the quotient by 2 until you get a quotient of zero
-                if (quotient != 0)
-                {
-                    Console.WriteLine("i: " + i);
-                    Console.WriteLine("quotient: " + quotient);
-                    
-                    
-                    quotient = quotient / 2;
-
-                    remainder = quotient % 2;
-                    Console.WriteLine(quotient % 2);
-                    Console.WriteLine("quo1 : " + quotient);
-                    Console.WriteLine("rem1: " + remainder);
-                    
-                }
-                else
-                {
-                    break;
-                }
-                i++;
-
+ 
+                remainder = quotient % 2;
+                quotient = quotient / 2;
+                result.Add(remainder);                                       
             }
-                
-               
-             
+            string binary = string.Empty;
+            for(var i = 0; i < result.Count; i++)
+            {
+                binary += result[result.Count - 1 - i]; 
+            }
+            Console.WriteLine(binary);
             
 
-                
-            
-            
+            /*
+            int value = 12;
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine("b: " + binary);
+            */
         }
+
+
+
+
+
+
+
+
+
     }
+    
 }
