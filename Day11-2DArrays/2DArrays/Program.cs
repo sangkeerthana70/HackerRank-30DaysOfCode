686bb933f592aeed4ab473b2b986ebb271b056fc
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-24 22:21:32 -0500

    Passed all test cases

--- Day11-2DArrays/2DArrays/Program.cs
+++ Day11-2DArrays/2DArrays/Program.cs
@@ -10,6 +10,7 @@ namespace TwoDArrays
     {
         static void Main(string[] args)
         {
+            // two dimensional array
             int[,] a = new int[6, 6] {
                 { 1, 1, 1, 0, 0, 0 },
                 { 0, 1, 0, 0, 0, 0 },
@@ -19,6 +20,7 @@ namespace TwoDArrays
                 { 0, 0, 0, 0, 0, 0 }
             };
             /*
+             * accessing rows and columns in a 2D-Array
             //Console.WriteLine(a.GetLength(0));
             for(var i = 0; i < a.GetLength(0); i++)
             {
@@ -30,6 +32,8 @@ namespace TwoDArrays
                 Console.WriteLine();
             }
             */
+
+            /* Old solution
             int hourGlassSum = 0;
             int maxValue = int.MinValue;
             // Outer loop to get upto 4 rows 
@@ -51,9 +55,38 @@ namespace TwoDArrays
 
             }
             Console.WriteLine("maxSum: " + maxValue);
+            */
+            // defines a array of arrays
+            int[][] arr = new int[6][];
 
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+            int hourGlassSum = 0;
+            int maxValue = int.MinValue;
+            for (var i = 0; i < arr.Length - 2; i++)
+            {
+                // Inner loop to get upto 4 columns 
+                for (var j = 0; j < arr[i].Length - 2; j++)
+                {
+                    //Console.WriteLine("({0},{1})",i,j);
+                    hourGlassSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    hourGlassSum += arr[i + 1][j + 1];
+                    hourGlassSum += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    //Console.WriteLine(hourGlassSum);
+                    if (hourGlassSum > maxValue)
+                    {
+                        maxValue = hourGlassSum;
+                    }
+                }
 
+            }
+            Console.WriteLine(maxValue);
 
         }
+
+
     }
+    
 }
