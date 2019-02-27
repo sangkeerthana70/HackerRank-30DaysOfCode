69ea385c1dc1c6019df6064b484ab10679b97f81
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-27 00:07:05 -0500

    Initial commit on the solution

---
+++ Day12/Day12/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    class Student: Person
+    {
+        string firstName;
+        string lastName;
+        int id;
+        int[] testScores;
+
+        public char Calculate(Student s)
+        {
+            return 'o';
+        }
+    }
+}
