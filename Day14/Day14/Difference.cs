6258c8d70f349729f1b6460d37538bfc9832c810
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-28 12:57:33 -0500

    Day-14 Scope passed all tests

---
+++ Day14/Day14/Difference.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    class Difference
+    {
+        private int[] elements;
+        // this property has scope of entire class, so the maximumDifference returned from method will be assigned to this property. 
+        public int maximumDifference;
+        
+        // Add your code here
+        public Difference(int [] e)
+        {
+            this.elements = e;
+            
+
+        }
+        // method to find the max difference
+        public void computeDifference()
+        {
+            int difference = 0;
+            maximumDifference = int.MinValue;
+            // access the array elements in int [] elements which has scope of entire class
+            for (var i = 0; i < elements.Length - 1; i++)
+            {
+                
+                //Console.WriteLine("i " + i);
+                //Console.WriteLine("elements[i] " + elements[i]);
+                for(var j = i+1; j < elements.Length; j++)
+                {
+                    //Console.WriteLine("j: " + j);
+                    //Console.WriteLine("elements[j] " + elements[j]);
+                    difference = Math.Abs(elements[i] - elements[j]);
+                    //difference = Math.Abs(difference);
+                    //Console.WriteLine("difference: " + difference);                   
+                    if (difference > maximumDifference)
+                    {
+                        maximumDifference = difference;                       
+                    }
+                    //Console.WriteLine("maxDiff: " + maximumDifference);
+                } 
+            }
+            //Console.WriteLine("maximumDifference: " + maximumDifference);
+        }
+
+    }
+}
---
+++ Day14/Day14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3 };
+            Difference d = new Difference(arr);
+            d.computeDifference();
+
+        }
+    }
+}
