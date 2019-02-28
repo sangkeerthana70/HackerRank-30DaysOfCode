2ac96f24dee910d633c426d64c922ff6a8e8a099
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-28 13:24:26 -0500

    Class Level Scope and Method Level Scope

--- Day14/ExampleOfClassLevelSCope/ExampleOfClassLevelSCope/Program.cs
+++ Day14/ExampleOfClassLevelSCope/ExampleOfClassLevelSCope/Program.cs
@@ -8,8 +8,9 @@ namespace ExampleOfClassLevelSCope
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Scope s = new Scope();
         }
     }
 }
