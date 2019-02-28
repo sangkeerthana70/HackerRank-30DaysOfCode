d0f9a81f71e6701258b68f626ed545e2ab7ceab6
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-28 13:07:28 -0500

    Initial commit

---
+++ Day14/ExampleOfClassLevelSCope/ExampleOfClassLevelSCope/Scope.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfClassLevelSCope
+{
+    public class Scope
+    {
+        bool b = true; // b1 has scope of entire class
+        int x = 88; // x1 has scope of entire class
+
+        Scope()
+        {
+            double d = 9.0;
+            example(d);
+            classVariable();
+        }
+
+        private void classVariable()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void example(double d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
