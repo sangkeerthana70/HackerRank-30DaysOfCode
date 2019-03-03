f132d863c8a065df0e2f39d3a8432aec9e0a578b
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-02 21:59:50 -0500

    Initial commit

---
+++ Day16/Day16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int convertToInt = int.Parse(S);
+            Console.WriteLine(convertToInt);
+        }
+    }
+}
