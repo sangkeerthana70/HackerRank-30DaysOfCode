0062d4db567a7bd56f9804c15b109d2025ae8915
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-03 13:22:07 -0500

    Day17 More exceptions solved

---
+++ Day17/Day17/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+    public class Calculator
+    {
+        int n;
+        int p;
+
+       //public  Calculator()
+       //{
+
+       //}
+
+        public int power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+            {
+                string Message = "n and p should be non-negative";
+                throw new System.ArgumentException(Message);
+                Console.WriteLine(Message);
+            }
+            int result = 1;
+            for (var i = 1; i <= p; i++)
+            {
+                Console.WriteLine("i: " + i);
+                result *= n ;
+                Console.WriteLine(result);
+            }
+            
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ Day17/Day17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator myCalculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+
+        }
+    }
+}
