91a0f058ebae8a228ba1d56618f0daa95fb2f7c9
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-03 13:24:39 -0500

    Passed all test cases

--- Day17/Day17/Calculator.cs
+++ Day17/Day17/Calculator.cs
@@ -18,12 +18,14 @@ namespace Day17
 
         public int power(int n, int p)
         {
+            // custom power method without using Math.Pow or recursion
             if (n < 0 || p < 0)
             {
                 string Message = "n and p should be non-negative";
                 throw new System.ArgumentException(Message);
                 Console.WriteLine(Message);
             }
+            // assign result to 1 to default the result of multiplying n ^ 0 to 1
             int result = 1;
             for (var i = 1; i <= p; i++)
             {
