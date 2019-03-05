109cab5668b0b082125bb3784b9dd96f14d53d3c
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-05 12:52:08 -0500

    Interfaces solved

---
+++ Day20/Day20/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            // Write Your Code Here
+
+            for (int i = 0; i < n; i++)
+            {
+                // Track number of elements swapped during a single array traversal
+                int numberOfSwaps = 0;
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (a[j] > a[j + 1])
+                    {
+                        swap(a[j], a[j + 1]);
+                        
+                        numberOfSwaps++;
+                    }
+                }
+
+                // If no elements were swapped during a traversal, array is sorted
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+
+                
+            }
+            
+
+            
+        }
+
+        private static int[] swap(int v1, int v2)
+        {
+            int[] newArr = new int[] { v2, v1 };
+            //foreach(var num in newArr)
+            //{
+            //    Console.WriteLine("num: " + num);
+            //}
+            return newArr;
+        }
+    }
+}
