44ff2b740d2904e7bf4684873aa4d990f0653aa0
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-06 12:51:46 -0500

    Day 20 w.ip

--- Day20/Day20/Program.cs
+++ Day20/Day20/Program.cs
@@ -13,21 +13,38 @@ namespace Day20
             int n = Convert.ToInt32(Console.ReadLine());
             string[] a_temp = Console.ReadLine().Split(' ');
             int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
             // Write Your Code Here
+            bubbleSort(a, n);
+            Console.WriteLine("Sorted array");
+        }
+
+       
+
+        public static void bubbleSort(int[] a, int n)
+        {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
 
             for (int i = 0; i < n; i++)
             {
-                // Track number of elements swapped during a single array traversal
-                int numberOfSwaps = 0;
+                
 
                 for (int j = 0; j < n - 1; j++)
                 {
+                    Console.WriteLine("Loop J: " + j);
                     // Swap adjacent elements if they are in decreasing order
                     if (a[j] > a[j + 1])
                     {
-                        swap(a[j], a[j + 1]);
-                        
+                        Console.WriteLine("Before swap: " + a[j] + " " + a[j + 1]);
+                        //swap(a[j], a[j + 1]);
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        Console.WriteLine("After swap: " + a[j] + " " +  a[j + 1]);
                         numberOfSwaps++;
+                        Console.WriteLine("Number of swaps: " + numberOfSwaps);
+
                     }
                 }
 
@@ -37,21 +54,29 @@ namespace Day20
                     break;
                 }
 
-                
             }
-            
 
-            
+            foreach(var snum in a)
+            {
+                Console.WriteLine("Each Elem: " + snum);
+            }
+
+            Console.WriteLine("Array is sorted in {0} swaps. " , numberOfSwaps);
+            //Console.WriteLine("noOfSwaps: " + numberOfSwaps);
+           
+            Console.WriteLine("First Element: {0} " + a[0]);
+            Console.WriteLine("Last Element: {0}" + a[a.Length-1]);
         }
 
-        private static int[] swap(int v1, int v2)
+        private static void PrintArray(int [] sortedArr)
         {
-            int[] newArr = new int[] { v2, v1 };
-            //foreach(var num in newArr)
-            //{
-            //    Console.WriteLine("num: " + num);
-            //}
-            return newArr;
+            for(var i = 0; i < sortedArr.Length -1; i++)
+            {
+                
+            }
+
+           
+
         }
     }
 }
