baf77fe522f6596d071a822db774f47dc132b3bf
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-01 11:22:29 -0400

    restructure Program.cs as in Hackerrank

--- Day22-BinarySearchTree/BinarySearchTree/Program.cs
+++ Day22-BinarySearchTree/BinarySearchTree/Program.cs
@@ -8,56 +8,50 @@ namespace BinarySearchTree
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            Node root = new Node(3);
-            //Console.WriteLine("root.data: " + root.data);
-            root.left = new Node(2);
-            //Console.WriteLine("root.left: " + root.left.data);
-            root.right = new Node(5);
-            //Console.WriteLine("root.right: " + root.right.data);
-            root.left.left = new Node(1);
-            //Console.WriteLine("root.left.left: " + root.left.left.data);
-            root.right.left = new Node(4);
-            //Console.WriteLine("root.right.left: " + root.right.left.data);
-            root.right.right = new Node(6);
-            //Console.WriteLine("root.right.right: " + root.right.right.data);
-            root.right.right.right = new Node(7);
-            //Console.WriteLine("root.right.right.right: " + root.right.right.right.data);
-            
-
-            int result = getHeight(root);
-            Console.WriteLine("result: " + result);
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
 
         }
 
-        public static int getHeight(Node root)
+        static int getHeight(Node root)
         {
             //Write your code here
-            int height = 0;
-            
             if (root == null) return -1;
+            return (1 + Math.Max(getHeight(root.left), getHeight(root.right)));
+        }
 
-            int left = getHeight(root.left);
-            Console.WriteLine("left: " + root.left);
-            int right = getHeight(root.right);
-            Console.WriteLine("right: " + root.right);
-
-            if (left > right)
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
             {
-                 height = 1 + left;
-                Console.WriteLine("If, height: " + height);
+                return new Node(data);
             }
             else
             {
-                 height = 1 + right;
-                Console.WriteLine("Else, height: " + height);
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
             }
 
-
-
-            //return Math.Max(left, right);
-            return height;
         }
     }
+
 }
