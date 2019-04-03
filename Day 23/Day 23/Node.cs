3bce6023c002d6a89378ae85c836b467a6c18953
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-03 10:37:23 -0400

    Added Day-23 BST Level-Order Traversal

---
+++ Day 23/Day 23/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_23
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+}
---
+++ Day 23/Day 23/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_23
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
+
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        //Write your code here
+        public static void levelOrder(Node root)
+        {
+
+            //use Queue data structure to traverse the binary tree and print the nodes
+            Queue<Node> myQ = new Queue<Node>();
+            //Node currNode;
+
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                myQ.Enqueue(root);
+            }
+
+            // while queue is not empty and has at least one node in it
+            while (myQ.Count > 0)
+            {
+                var nodeInFrontOfQueue = myQ.Peek();
+                //Console.WriteLine(nodeInFrontOfQueue.GetType());
+                Console.Write(nodeInFrontOfQueue.data + " ");
+                //currNode = myQ.Dequeue();
+                //Console.WriteLine("Visit current node and print its value: " + currNode.data);
+
+                if (nodeInFrontOfQueue.left != null)
+                {
+                    myQ.Enqueue(nodeInFrontOfQueue.left);
+                }
+                if (nodeInFrontOfQueue.right != null)
+                {
+                    myQ.Enqueue(nodeInFrontOfQueue.right);
+                }
+                myQ.Dequeue();
+                //Console.WriteLine("No of elements in the queue: {0}", myQ.Count);
+
+            }
+        }
+    }
+}
